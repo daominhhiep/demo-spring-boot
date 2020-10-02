84b86ef341b4c120aa60e201b2f37c633de896f8
admin <daominhhiep97@gmail.com>
2020-10-02 09:24:31 +0700

    [Thực hành] tạo ứng dụng web đơn giản với Spring Boot

---
+++ src/main/java/com/codegym/demospringboot/controller/GreetingController.java
@@ -0,0 +1,12 @@
+package com.codegym.demospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/")
+    public String getHomePage() {
+        return "home";
+    }
+}
